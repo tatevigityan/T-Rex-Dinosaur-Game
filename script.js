07435caaf7b5c4b714a03a8e47c739849c737cca
tatev <tatev.igityan@bk.ru>
2021-11-06 15:42:02 +0300

    Second commit

--- script.js
+++ script.js
@@ -12,17 +12,13 @@ function jump() {
 }
 
 let isAlive = setInterval(function () {
-  // get current dino Y position
   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
 
-  // get current cactus X position
   let cactusLeft = parseInt(
     window.getComputedStyle(cactus).getPropertyValue("left")
   );
 
-  // detect collision
   if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-    // collision
     alert("Game Over!");
   }
 }, 10);
